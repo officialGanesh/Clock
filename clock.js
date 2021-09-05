835679262e2227f0a0d4fa6eb37b51d904bd43b3
Ganesh <ganuofficial2795@gmail.com>
2021-09-05 20:46:25 +0530

    Noon condition and Time

--- clock.js
+++ clock.js
@@ -1,2 +1,41 @@
 
 console.log("Clock Using Javacript 🚀");
+let hour = document.getElementById('hour');
+let min = document.getElementById('min');
+let sec = document.getElementById('sec');
+let noon = document.getElementById('noon');
+
+
+function clock(){
+    let newDate = new Date();
+    let  hr = newDate.getHours();
+    let  mn = newDate.getMinutes();
+    let  sc = newDate.getSeconds();
+    
+    hour.innerHTML = hr;
+    min.innerHTML = mn;
+    sec.innerHTML = sc;
+    
+    if(hr===0){
+        hr = 12;
+        hour.innerHTML = hr;
+    };
+
+    if(hr<12){
+        hour.innerHTML = `0${hr}`;
+        noon.innerHTML = "AM"
+    };
+
+    if(hr>12){
+        hour.innerHTML = `0${hr-12}`;
+        noon.innerHTML = "PM"
+    };
+
+};
+
+function mainFunction(){
+    setInterval("clock()",1000);
+};
+
+
+mainFunction()
